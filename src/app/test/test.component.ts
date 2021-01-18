fd01e1fbd6530c136c5e11dd44bfd5756022855f
leesohyun129 <leesohyun129@naver.com>
2021-01-18 17:21:03 +0900

    답안 작성후 버튼통해 작성한 답안 확인

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { chapter1_test } from '../question-list/chapter1-test';
+import { FormGroup, FormBuilder, Validator } from '@angular/forms';
 import { chapter2_test } from '../question-list/chapter2-test';
 @Component({
   selector: 'app-test',
@@ -7,6 +8,15 @@ import { chapter2_test } from '../question-list/chapter2-test';
   styleUrls: ['./test.component.css']
 })
 export class TestComponent {
-  
-  chapter1_test = chapter1_test; //배열을 사용해보고싶다....
+  answer : string ="";
+  examForm = this.formBuilder.group({
+    id: 0,
+    answer: ''
+  })
+  chapter1_test = chapter1_test; //배열을 사용하고싶다....
+  constructor(private formBuilder: FormBuilder){
+  }
+  onExam(){
+    window.alert(JSON.stringify(this.examForm.value.answer));
+  }
 }
